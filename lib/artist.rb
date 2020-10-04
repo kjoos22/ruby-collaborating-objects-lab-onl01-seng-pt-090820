bd1918b4dbbad10d156d16a3ccd46f8e706f70cd
Kristian Joos <kjoos22@gmail.com>
2020-10-04 20:19:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,12 +23,5 @@ class Artist
   def self.find_or_create_by_name(name)
     artists = Artist.all
     
-    selected_artist = artists.select |artist|
-      artist.name == name
-    end
-    if selected_artist == nil
-      selected_artist = Artist.new(name)
-    end
-    selected_artist
   end
 end
