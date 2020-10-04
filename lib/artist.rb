47396cc9c3f4481badbfd3203309a098c44d75d8
Kristian Joos <kjoos22@gmail.com>
2020-10-04 20:23:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,8 +22,8 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    artist = Artist.all.select {|artist| artist.name == name}
-    binding.pry
+    artists = Artist.all.select {|artist| artist.name == name}
+    artist = artists[0]
     if artist == nil
       artist = artist.new(name)
     end
