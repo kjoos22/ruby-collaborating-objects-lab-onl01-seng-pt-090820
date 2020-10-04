acf984315f3f64557c98ef3d5fe95cef1d1c3a95
Kristian Joos <kjoos22@gmail.com>
2020-10-04 20:05:08 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end  
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def find_or_create_by_name(name)
+    Artist.all.select {|artist| artist.name == name}
+  end
+end
