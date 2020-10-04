33ad4e874a235a85ae87e4d07a27c4b25ebd0f36
Kristian Joos <kjoos22@gmail.com>
2020-10-04 20:50:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,7 @@ class Song
     filename = filename.split(" - ")
     song = Song.new(filename[1])
     song.artist = Artist.find_or_create_by_name(filename[0])
+    song.artist.add_song(song)
     song
   end  
     
